6f0f74178433599c68a443b57d04d3c48abf8b5b
MariaGillVarghese <mariagillvarghese@gmail.com>
2026-03-09 22:19:51 +0100

    Update main.tf

--- terraform/e-commerce-cluster/modules/ec2/main.tf
+++ terraform/e-commerce-cluster/modules/ec2/main.tf
@@ -1,3 +1,8 @@
+resource "aws_key_pair" "jenkins" {
+  key_name   = "jenkins-key"
+  public_key = file("~/.ssh/id_rsa.pub")  # Path to public key on Jenkins server
+}
+
 resource "aws_instance" "this" {
   ami           = var.ami_id
   instance_type = var.instance_type
@@ -8,7 +13,7 @@ resource "aws_instance" "this" {
 
   vpc_security_group_ids = [var.security_group_id]
 
-  key_name = var.key_name
+  key_name = aws_key_pair.jenkins.key_name
 
   tags = {
     Name = var.instance_name
