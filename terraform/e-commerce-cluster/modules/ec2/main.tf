2c27dc59ccbea23ae7be9afbcbd609d88163fb7b
MariaGillVarghese <mariagillvarghese@gmail.com>
2026-03-13 17:26:29 +0100

    Update main.tf

--- terraform/e-commerce-cluster/modules/ec2/main.tf
+++ terraform/e-commerce-cluster/modules/ec2/main.tf
@@ -1,14 +1,91 @@
-# Create multiple EC2s
-resource "aws_instance" "this" {
-  ami             = var.ami_id
-  instance_type   = var.instance_type
-  subnet_id       = var.subnet_id
-  associate_public_ip_address = true
-  vpc_security_group_ids      = [var.security_group_id]
+terraform {
+  required_version = ">= 1.5.0"
 
-  key_name = var.key_name
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+     bucket         = "e-commerce-sys"
+     key            = "cluster/terraform.tfstate"
+     region         = "us-east-1"
+     dynamodb_table = "terraform-lock"
+     encrypt        = true
+   }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# ------------------------------
+# Get latest Amazon Linux 2023 AMI
+# ------------------------------
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# ------------------------------
+# Get VPC and Subnet
+# ------------------------------
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket = "e-commerce-sys"
+    key    = "vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+# ------------------------------
+# Ecommerce Node 1
+# ------------------------------
+resource "aws_instance" "ecommerce_node1" {
+  ami                    = data.aws_ami.amazon_linux_2023.id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = data.terraform_remote_state.vpc.outputs.public_subnet_id
+  vpc_security_group_ids = [aws_security_group.ecommerce.id]
+
+  associate_public_ip_address = true
 
   tags = {
-    Name = "${var.instance_name}"
+    Name    = "ecommerce-node1"
+    Project = var.project_name
+    Role    = "ecommerce-cluster"
+  }
+}
+
+# ------------------------------
+# Ecommerce Node 2
+# ------------------------------
+resource "aws_instance" "ecommerce_node2" {
+  ami                    = data.aws_ami.amazon_linux_2023.id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = data.terraform_remote_state.vpc.outputs.public_subnet_id
+  vpc_security_group_ids = [aws_security_group.ecommerce.id]
+
+  associate_public_ip_address = true
+
+  tags = {
+    Name    = "ecommerce-node2"
+    Project = var.project_name
+    Role    = "ecommerce-cluster"
   }
 }
