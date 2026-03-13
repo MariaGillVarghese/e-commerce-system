74be7a45a78bb47902cfcdf16b0d6619520f3415
MariaGillVarghese <mariagillvarghese@gmail.com>
2026-03-13 17:26:43 +0100

    Update outputs.tf

--- terraform/e-commerce-cluster/modules/ec2/outputs.tf
+++ terraform/e-commerce-cluster/modules/ec2/outputs.tf
@@ -1,14 +1,34 @@
-output "public_ips" {
-  description = "Public IPs of EC2 instances"
-  value       = aws_instance.this[*].public_ip
+output "ecommerce_node1_instance_id" {
+  description = "Instance ID of ecommerce node 1"
+  value       = aws_instance.ecommerce_node1.id
 }
 
-output "instance_ids" {
-  description = "IDs of EC2 instances"
-  value       = aws_instance.this[*].id
+output "ecommerce_node1_public_ip" {
+  description = "Public IP of ecommerce node 1"
+  value       = aws_instance.ecommerce_node1.public_ip
 }
 
-output "private_ips" {
-  description = "Private IPs of EC2 instances"
-  value       = aws_instance.this[*].private_ip
+output "ecommerce_node1_private_ip" {
+  description = "Private IP of ecommerce node 1"
+  value       = aws_instance.ecommerce_node1.private_ip
+}
+
+output "ecommerce_node2_instance_id" {
+  description = "Instance ID of ecommerce node 2"
+  value       = aws_instance.ecommerce_node2.id
+}
+
+output "ecommerce_node2_public_ip" {
+  description = "Public IP of ecommerce node 2"
+  value       = aws_instance.ecommerce_node2.public_ip
+}
+
+output "ecommerce_node2_private_ip" {
+  description = "Private IP of ecommerce node 2"
+  value       = aws_instance.ecommerce_node2.private_ip
+}
+
+output "ecommerce_security_group_id" {
+  description = "Security group ID of the ecommerce cluster"
+  value       = aws_security_group.ecommerce.id
 }
