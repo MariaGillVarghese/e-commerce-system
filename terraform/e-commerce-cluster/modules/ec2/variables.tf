77e3729299663715f714d2d19f14e40909cbda6f
MariaGillVarghese <mariagillvarghese@gmail.com>
2026-03-13 17:27:52 +0100

    Update variables.tf

--- terraform/e-commerce-cluster/modules/ec2/variables.tf
+++ terraform/e-commerce-cluster/modules/ec2/variables.tf
@@ -1,13 +1,35 @@
-variable "instance_name" { type = string }
-variable "ami_id" { type = string }
-variable "instance_type" { type = string }
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
 
-variable "subnet_id" { type = string }
+variable "instance_type" {
+  description = "EC2 instance type for ecommerce nodes"
+  type        = string
+  default     = "t3.small"
+}
 
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  type        = string
+  default     = "e-commerce-key-pair"
+}
 
-variable "key_name" { type = string }
-variable "vpc_id" { type = string }
+variable "developer_ip" {
+  description = "Developer public IP for SSH access (CIDR format, e.g. 1.2.3.4/32). Use 0.0.0.0/0 for development."
+  type        = string
+  default     = "0.0.0.0/0"
+}
 
-variable "security_group_id" {
-  type = string
+variable "jenkins_security_group_id" {
+  description = "Security group ID of the Jenkins server (for SSH access)"
+  type        = string
+  default     = "sg-00dd23f0dd812ed2e"
+}
+
+variable "project_name" {
+  description = "Project name used for tagging"
+  type        = string
+  default     = "ecommerce-ha"
 }
